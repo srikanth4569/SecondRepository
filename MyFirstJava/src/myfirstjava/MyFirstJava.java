3dc04a5fa3921f236daaa830d7a0c09f5de34e0b
Bhumpalli <S542265@nwmissouri.edu>
2021-08-22 11:55:15 -0500

    My First comit

---
+++ MyFirstJava/src/myfirstjava/MyFirstJava.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstjava;
+
+/**
+ *
+ * @author S542265
+ */
+public class MyFirstJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("myfirstjava.MyFirstJava.main()");
+        // TODO code application logic here
+    }
+    
+}
